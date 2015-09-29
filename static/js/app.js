ef3a5888109cd0825727ccaa0a372bec05c95499
cattyz113 <cattyz113@gmail.com>
2015-09-29 16:28:19 -0500

    Agrego los contenidos

--- static/js/app.js
+++ static/js/app.js
@@ -6,11 +6,35 @@ angular.module('Sistema')
 })
 .controller("rolesController",function($scope, $http, $routeParams){
 })
+.controller("usuariosController",function($scope, $http, $routeParams){
+})
+.controller("estilosController",function($scope, $http, $routeParams){
+})
+.controller("seguridadController",function($scope, $http, $routeParams){
+})
+.controller("peticionesController",function($scope, $http, $routeParams){
+})
 .controller("planificacionController",function($scope, $http, $routeParams){
+})
+.controller("localesController",function($scope, $http, $routeParams){
+})
+.controller("gatencionController",function($scope, $http, $routeParams){
+})
+.controller("logicaController",function($scope, $http, $routeParams){
 })
 .controller("videoController",function($scope, $http, $routeParams){
 })
+.controller("playlistController",function($scope, $http, $routeParams){
+})
 .controller("atencionController",function($scope, $http, $routeParams){
+})
+.controller("mod_atencion5Controller",function($scope, $http, $routeParams){
+})
+.controller("supervisionController",function($scope, $http, $routeParams){
+})
+.controller("crit_atencionController",function($scope, $http, $routeParams){
+})
+.controller("msj_alertaController",function($scope, $http, $routeParams){
 })
 .controller("reportesController",function($scope, $http, $routeParams){
 });
--- static/js/router.js
+++ static/js/router.js
@@ -14,17 +14,65 @@ angular.module('Sistema',['ngRoute'])
 			controller:		"rolesController",
 			templateUrl:	"./templates/configuracion/roles.html"
 		})
+		.when("/configurar/usuarios",{
+			controller:		"usuariosController",
+			templateUrl:	"./templates/configuracion/usuarios.html"
+		})
+		.when("/configurar/estilos",{
+			controller:		"estilosController",
+			templateUrl:	"./templates/configuracion/estilos.html"
+		})
+		.when("/configurar/seguridad",{
+			controller:		"seguridadController",
+			templateUrl:	"./templates/configuracion/seguridad.html"
+		})
+		.when("/configurar/peticiones",{
+			controller:		"peticionesController",
+			templateUrl:	"./templates/configuracion/peticiones.html"
+		})
 		.when("/planificacion",{
 			controller:		"planificacionController",
 			templateUrl:	"./templates/planificacion/index.html"
+		})
+		.when("/planificacion/locales",{
+			controller:		"localesController",
+			templateUrl:	"./templates/planificacion/locales.html"
+		})
+		.when("/planificacion/gatencion",{
+			controller:		"gatencionController",
+			templateUrl:	"./templates/planificacion/gatencion.html"
+		})
+		.when("/planificacion/logica",{
+			controller:		"logicaController",
+			templateUrl:	"./templates/planificacion/logica.html"
 		})
 		.when("/video",{
 			controller:		"videoController",
 			templateUrl:	"./templates/video/index.html"
 		})
+		.when("/video/playlist",{
+			controller:		"playlistController",
+			templateUrl:	"./templates/video/playlist.html"
+		})
 		.when("/atencion",{
 			controller:		"atencionController",
 			templateUrl:	"./templates/atencion/index.html"
+		})
+		.when("/atencion/mod_atencion5",{
+			controller:		"mod_atencion5Controller",
+			templateUrl:	"./templates/atencion/mod_atencion5.html"
+		})
+		.when("/atencion/supervision",{
+			controller:		"supervisionController",
+			templateUrl:	"./templates/atencion/supervision.html"
+		})
+		.when("/atencion/crit_atencion",{
+			controller:		"crit_atencionController",
+			templateUrl:	"./templates/atencion/crit_atencion.html"
+		})
+		.when("/atencion/msj_alerta",{
+			controller:		"msj_alertaController",
+			templateUrl:	"./templates/atencion/msj_alerta.html"
 		})
 		.when("/reportes",{
 			controller:		"reportesController",
