5a2850a8e4c83687b86d6a017fcef045069315d4
Daniel Salazar <shenaider14@hotmail.com>
2015-10-26 17:29:59 -0500

    Agregando Slide flotante a configuración, planificación y atención.

--- static/js/router.js
+++ static/js/router.js
@@ -72,7 +72,7 @@ angular.module('Sistema',['ngRoute'])
 		})
 		.when("/atencion/mod_atencion2",{
 			controller:		"mod_atencion2Controller",
-			templateUrl:	"./templates/atencion/mod_atencion5.html"
+			templateUrl:	"./templates/atencion/mod_atencion4.html"
 		})
 		.when("/atencion/supervision",{
 			controller:		"supervisionController",
