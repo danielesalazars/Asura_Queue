4878a3110beccd305ec03fb7ad76458b2c303958
Daniel Salazar <shenaider14@hotmail.com>
2015-10-26 07:07:50 -0500

    Agregando Slide flotante a la zona de configuración y modificando los index de cada menu para que abarque en toda la resolucion.

--- static/js/global.js
+++ static/js/global.js
@@ -25,6 +25,7 @@ $(document).ready(function(){
 
 	$("#logo a").click(function(){
 		$(".contenedor_sidebar").removeClass('block_imp');
+		$("a").removeClass("active");
 		activa_home();
 	});
 	
@@ -32,9 +33,9 @@ $(document).ready(function(){
 
 function activa_home(){
 	$(".contenedor_sidebar").addClass('none');
-	$(".cuerpo").removeClass("col-lg-9 col-md-9 col-lg-offset-2 col-md-offset-3");
+	$(".cuerpo").removeClass("col-lg-10 col-md-9 col-lg-offset-2 col-md-offset-3");
 	$(".cuerpo").addClass("col-lg-12 col-md-12");
-	$(".cuerpo_pagina").removeClass('col-lg-8 col-md-12 col-lg-offset-2');
+	$(".cuerpo_pagina").removeClass('col-lg-12 col-md-12');
 	$(".cuerpo_pagina").addClass('col-lg-7 col-md-9 col-lg-offset-3 col-md-offset-2');
 	$(".subir").removeClass("subir_active");
 }
@@ -51,6 +52,9 @@ function activar_sub_menu(item_nav,sub_item_nav){
 	$(sub_item_nav+" a").on("click",function(){
 		$(".sub_sidebar ul,.sub_sidebar a").removeClass("active");
 		$("#sidebar_left a").removeClass("active2");
+		$(".cuerpo").addClass('col-lg-10 col-md-9 col-lg-offset-2 col-md-offset-3');
+		$(".cuerpo_pagina").removeClass('col-lg-7 col-md-9 col-lg-offset-3 col-md-offset-3');
+		$(".cuerpo_pagina").addClass('col-lg-12 col-md-12');
 		$(sub_item_nav).addClass("active");
 		$(this).addClass('active');
 		$(item_nav).addClass('active2');
@@ -59,12 +63,16 @@ function activar_sub_menu(item_nav,sub_item_nav){
 
 function change_sub_menu(){
 	$("#sidebar_left ul a, .sub_sidebar ul a").on("mouseover",function(){
-
-		$(".cuerpo").removeClass("col-lg-12 col-md-12");
-		$(".cuerpo").addClass("col-lg-9 col-md-9 col-lg-offset-2 col-md-offset-3");
-
-		$(".cuerpo_pagina").removeClass('col-lg-7 col-md-9 col-lg-offset-3 col-md-offset-2');
-		$(".cuerpo_pagina").addClass('col-lg-8 col-md-12 col-lg-offset-2');
+		if(window.location.hash == "#/"){
+			$(".cuerpo_pagina").removeClass('col-md-offset-2');
+			$(".cuerpo_pagina").addClass('col-md-offset-3');
+		}else{
+			$(".cuerpo").removeClass("col-lg-12 col-md-12");
+			$(".cuerpo").addClass("col-lg-10 col-md-9 col-lg-offset-2 col-md-offset-3");
+			$(".cuerpo_pagina").removeClass('col-lg-7 col-md-9 col-lg-offset-3 col-md-offset-2');
+			$(".cuerpo_pagina").removeClass('col-md-offset-3');
+			$(".cuerpo_pagina").addClass('col-lg-12 col-md-12');
+		}
 
 		$(".contenedor_sidebar").addClass("block_imp");
 		$(".subir").addClass("subir_active");
