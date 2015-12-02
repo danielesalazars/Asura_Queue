60cf5126649eb4c863ffa724ec20ae8f9c2559ea
Daniel Salazar <shenaider14@hotmail.com>
2015-12-02 10:07:47 -0500

    Eliminando archivos innecesarios del proyecto.

---
+++ static/js/app.js
@@ -0,0 +1,47 @@
+angular.module('Sistema')
+
+.controller("homeController",function($scope, $http, $routeParams){
+})
+/* Configuración */
+.controller("configurarController",function($scope, $http, $routeParams){
+})
+.controller("rolesController",function($scope, $http, $routeParams){
+})
+.controller("usuariosController",function($scope, $http, $routeParams){
+})
+.controller("estilosController",function($scope, $http, $routeParams){
+})
+.controller("seguridadController",function($scope, $http, $routeParams){
+})
+.controller("peticionesController",function($scope, $http, $routeParams){
+})
+/* Planificación */
+.controller("planificacionController",function($scope, $http, $routeParams){
+})
+.controller("localesController",function($scope, $http, $routeParams){
+})
+.controller("gatencionController",function($scope, $http, $routeParams){
+})
+.controller("logicaController",function($scope, $http, $routeParams){
+})
+/* Video */
+.controller("videoController",function($scope, $http, $routeParams){
+})
+.controller("playlistController",function($scope, $http, $routeParams){
+})
+/* Atención */
+.controller("atencionController",function($scope, $http, $routeParams){
+})
+.controller("mod_atencion2Controller",function($scope, $http, $routeParams){
+})
+.controller("supervisionController",function($scope, $http, $routeParams){
+})
+.controller("crit_atencionController",function($scope, $http, $routeParams){
+})
+.controller("msj_alertaController",function($scope, $http, $routeParams){
+})
+/* Reporte */
+.controller("reportesController",function($scope, $http, $routeParams){
+})
+.controller("clt_atendidosController",function($scope, $http, $routeParams){
+});
