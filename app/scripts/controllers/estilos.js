0d82414856c64613b6d0b2edc5e48c1e2bff0070
cattyz113 <cattyz113@gmail.com>
2016-02-11 18:59:17 -0500

    Modificacion de estilos / Configuracion

--- app/scripts/controllers/estilos.js
+++ app/scripts/controllers/estilos.js
@@ -11,6 +11,9 @@
  .controller('EstilosCtrl',['$scope','$rootScope','$routeParams', function ($scope, $rootScope, $routeParams) {
 	minicolors();
 	estilos_kiosco_html();
+	estilos_identificacion_html();
+	estilos_escaneo_html();
+	estilos_menu_html()
 	estilos_video_html();
 	estilos_multi_page_html();
 	estilos_kiosco();
@@ -22,12 +25,40 @@
 	}
 
     function estilos_kiosco_html(){
-	 	if( $routeParams.estilo == "kioscos_generales"  ||
-	 		$routeParams.estilo == "kioscos_textos"     ||
-	 		$routeParams.estilo == "kioscos_botones"    ||
-	 		$routeParams.estilo == "kioscos_menu"        ){
+	 	if( $routeParams.estilo == "bienvenida_imagenes"  	||
+	 		$routeParams.estilo == "bienvenida_textos"     	||
+	 		$routeParams.estilo == "bienvenida_botones"   	||
+	 		$routeParams.estilo == "bienvenida_boton_ticket"||
+	 		$routeParams.estilo == "bienvenida_mensajes"        ){
 
 	 		$scope.estilo = "views/configuracion/estilos/kioscos.html";
+	 	}
+    }
+
+    function estilos_identificacion_html(){
+	 	if( $routeParams.estilo == "identificacion_imagenes"  	||
+	 		$routeParams.estilo == "identificacion_textos"  	||
+	 		$routeParams.estilo == "identificacion_botones"        ){
+
+	 		$scope.estilo = "views/configuracion/estilos/identificacion.html";
+	 	}
+    }
+
+    function estilos_escaneo_html(){
+	 	if( $routeParams.estilo == "escaneo_imagenes"  	||
+	 		$routeParams.estilo == "escaneo_textos"        ){
+
+	 		$scope.estilo = "views/configuracion/estilos/escaneo.html";
+	 	}
+    }
+
+    function estilos_menu_html(){
+	 	if( $routeParams.estilo == "menu_imagenes"  	||
+	 		$routeParams.estilo == "menu_botones"   	||
+	 		$routeParams.estilo == "menu_mensajes"||
+	 		$routeParams.estilo == "menu_administracion"        ){
+
+	 		$scope.estilo = "views/configuracion/estilos/menu.html";
 	 	}
     }
 
@@ -44,29 +75,72 @@
 				$scope.estilo = "views/configuracion/estilos/consola.html";
 				break;
 			/* KIOSCOS */
-			case "kioscos_generales":
-				$scope.img_estilos_kioscos  = 'images/img_generales.png';
-				$scope.estilos_kiosco       = 'views/configuracion/estilos/kioscos/generales.html';
+			case "bienvenida_imagenes":
+				$scope.img_estilos_kioscos  = 'images/qmatiq_bienvenido-generales.png';
+				$scope.estilos_kiosco       = 'views/configuracion/estilos/kioscos/imagenes.html';
 				break;
-			case "kioscos_textos":
-				$scope.img_estilos_kioscos  = 'images/img_generales.png';
+			case "bienvenida_textos":
+				$scope.img_estilos_kioscos  = 'images/qmatiq_bienvenido-generales.png';
 				$scope.estilos_kiosco       = 'views/configuracion/estilos/kioscos/textos.html';
 				break;
-			case "kioscos_botones":
-				$scope.img_estilos_kioscos  = 'images/img_generales.png';
+			case "bienvenida_botones":
+				$scope.img_estilos_kioscos  = 'images/qmatiq_bienvenido-generales.png';
 				$scope.estilos_kiosco       = 'views/configuracion/estilos/kioscos/botones.html';
 				break;
-			case "kioscos_menu":
-				$scope.img_estilos_kioscos  = 'images/img_kiosco.png';
-				$scope.estilos_kiosco       = 'views/configuracion/estilos/kioscos/menu.html';
+			case "bienvenida_boton_ticket":
+				$scope.img_estilos_kioscos  = 'images/qmatiq_bienvenido-generales.png';
+				$scope.estilos_kiosco       = 'views/configuracion/estilos/kioscos/boton_ticket.html';
+				break;
+			case "bienvenida_mensajes":
+				$scope.img_estilos_kioscos  = 'images/qmatiq_bienvenido-mensajes.png';
+				$scope.estilos_kiosco       = 'views/configuracion/estilos/kioscos/mensajes.html';
+				break;
+			/* IDENTIFICACION */
+			case "identificacion_imagenes":
+				$scope.img_estilos_kioscos  	= 'images/qmatiq_identificacion-imagenes.png';
+				$scope.estilos_identificacion 	= 'views/configuracion/estilos/identificacion/imagenes.html';
+				break;
+			case "identificacion_textos":
+				$scope.img_estilos_kioscos  	= 'images/qmatiq_identificacion-imagenes.png';
+				$scope.estilos_identificacion  	= 'views/configuracion/estilos/identificacion/textos.html';
+				break;
+			case "identificacion_botones":
+				$scope.img_estilos_kioscos  	= 'images/qmatiq_identificacion-imagenes.png';
+				$scope.estilos_identificacion 	= 'views/configuracion/estilos/identificacion/botones.html';
+				break;
+			/* ESCANEO */
+			case "escaneo_imagenes":
+				$scope.img_estilos_kioscos  = 'images/qmatiq_escaneo-imagenes.png';
+				$scope.estilos_escaneo 		= 'views/configuracion/estilos/escaneo/imagenes.html';
+				break;
+			case "escaneo_textos":
+				$scope.img_estilos_kioscos  = 'images/qmatiq_escaneo-imagenes.png';
+				$scope.estilos_escaneo  	= 'views/configuracion/estilos/escaneo/textos.html';
+				break;
+			/* MENU */
+			case "menu_imagenes":
+				$scope.img_estilos_kioscos  = 'images/qmatiq_MenuAdmin-imagenes.png';
+				$scope.estilos_menu 		= 'views/configuracion/estilos/menu/imagenes.html';
+				break;
+			case "menu_botones":
+				$scope.img_estilos_kioscos  = 'images/qmatiq_MenuAdmin-imagenes.png';
+				$scope.estilos_menu 		= 'views/configuracion/estilos/menu/botones.html';
+				break;
+			case "menu_mensajes":
+				$scope.img_estilos_kioscos  = 'images/qmatiq_MenuAdmin-Mensajes.png';
+				$scope.estilos_menu 		= 'views/configuracion/estilos/menu/mensajes.html';
+				break;
+			case "menu_administracion":
+				$scope.img_estilos_kioscos  = 'images/qmatiq_MenuAdmin-Admin.png';
+				$scope.estilos_menu 		= 'views/configuracion/estilos/menu/administracion.html';
 				break;
 			/* VIDEOS */
 			case "videos_generales":
-				$scope.img_estilos_kioscos  = 'images/img_presentacion.png';
-				$scope.estilos_video       = 'views/configuracion/estilos/videos/generales.html';
+				$scope.img_estilos_kioscos  = 'images/qmatiq_video-formato.png';
+				$scope.estilos_video       = 'views/configuracion/estilos/videos/formatos.html';
 				break;
 			case "videos_colores":
-				$scope.img_estilos_kioscos  = 'images/img_presentacion.png';
+				$scope.img_estilos_kioscos  = 'images/qmatiq_video-formato.png';
 				$scope.estilos_video       = 'views/configuracion/estilos/videos/colores.html';
 				break;
 			/* MOVILES */
