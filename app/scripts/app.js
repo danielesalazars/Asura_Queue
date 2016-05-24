c1d5da8bfbbb719403bf1ce879f623c9bc7de64b
cattyz113 <cattyz113@gmail.com>
2016-05-24 11:24:46 -0500

    Cambios estilos

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,15 +26,15 @@
   'chart.js',
   'googlechart'
   ])
-/* 
+ 
 .constant('URL',{ 
   'path' : 'http://api.asura.piensamasalla.com/' 
 })
-*/
+/*
 .constant('URL',{ 
   'path' : 'http://localhost/asura-api/' 
 })
- 
+ */
 
 .config(function(uiGmapGoogleMapApiProvider) {
   uiGmapGoogleMapApiProvider.configure({
